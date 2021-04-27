7e36c65187dd42aa9c3c589a6c45be689ba1b6f4
Vyas Ramankulangara <vyas01890@outlook.com>
2021-04-27 14:36:27 -0500

    remove sentry config

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { Integrations } from '@sentry/tracing';
 import App from './App';
 import './index.css';
 
-Sentry.init({
-	dsn: process.env.REACT_APP_SENTRY_PUBLIC_URL,
-	integrations: [new Integrations.BrowserTracing()],
-	tracesSampleRate: 1.0,
-});
+// Sentry.init({
+// 	dsn: process.env.REACT_APP_SENTRY_PUBLIC_URL,
+// 	integrations: [new Integrations.BrowserTracing()],
+// 	tracesSampleRate: 1.0,
+// });
 
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
