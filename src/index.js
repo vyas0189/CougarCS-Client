ce390560be802246660f79ad93af4c86cd19312e
Vyas Ramankulangara <vyas01890@outlook.com>
2021-04-27 15:53:47 -0500

    async await

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { Integrations } from '@sentry/tracing';
 import App from './App';
 import './index.css';
 
-(async () => {
+(() => {
 	try {
-		await Sentry.init({
+		Sentry.init({
 			dsn: process.env.REACT_APP_SENTRY_PUBLIC_URL,
 			integrations: [new Integrations.BrowserTracing()],
 			tracesSampleRate: 1.0,
