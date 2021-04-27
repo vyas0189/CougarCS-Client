09aa0faef7e1d3a9ad52f2dec3dae21d05bb237a
Vyas Ramankulangara <vyas01890@outlook.com>
2021-04-27 15:50:05 -0500

    try catch

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
-// import * as Sentry from '@sentry/react';
-// import { Integrations } from '@sentry/tracing';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
 import './index.css';
 
-// (async () => {
-// 	console.log('Sentry...');
-// 	await Sentry.init({
-// 		dsn: process.env.REACT_APP_SENTRY_PUBLIC_URL,
-// 		integrations: [new Integrations.BrowserTracing()],
-// 		tracesSampleRate: 1.0,
-// 	});
-// })();
+(async () => {
+	try {
+		await Sentry.init({
+			dsn: process.env.REACT_APP_SENTRY_PUBLIC_URL,
+			integrations: [new Integrations.BrowserTracing()],
+			tracesSampleRate: 1.0,
+		});
+	} catch (e) {
+		console.log(e);
+	}
+})();
 
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
