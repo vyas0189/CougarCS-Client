0c5f81ee94ea3ae0e6fc8185779db06db86de733
Vyas Ramankulangara <vyas01890@outlook.com>
2021-04-27 12:01:34 -0500

    fix merge issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
-<<<<<<< HEAD
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import App from './App';
@@ -12,11 +11,6 @@ Sentry.init({
 	tracesSampleRate: 1.0,
 });
 
-=======
-import App from './App';
-import './index.css';
-
->>>>>>> 6be3c2e0a34d03ff41e186acb49f67a3fc28b7c7
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
 	hydrate(<App />, rootElement);
